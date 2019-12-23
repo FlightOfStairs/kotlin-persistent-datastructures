623503f0c77feeae1e57e299c5787c8398668dde
Alistair Smith <smithali@amazon.com>
2019-12-23 18:13:13 +0000

    Inital commit with build config

---
+++ README.md
@@ -0,0 +1,3 @@
+# kotlin-persistent-datastructures
+
+Toy project, not expected to be useful. Plan is to implement several [persistent datastructures](https://en.wikipedia.org/wiki/Persistent_data_structure) in kotlin as practise with each.
---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    id("io.gitlab.arturbosch.detekt") version "1.3.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("com.github.ben-manes.versions") version "0.27.0"
+}
+
+repositories {
+    jcenter()
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.3.0")
+}
+
+detekt {
+    toolVersion = "1.0.0-RC14"
+    input = files("src/main/kotlin", "src/test/kotlin")
+
+    config = files(projectDir.resolve("detekt-config.yml"))
+    buildUponDefaultConfig = true
+
+    baseline = projectDir.resolve("detekt-baseline.xml")
+
+    failFast = false
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    exclude(".*/resources/.*,.*/build/.*")
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "6.0.1"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
